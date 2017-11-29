403479316cf5d662025de3905f8822d550fc2099
Jessica L.T. Martin <apersonofnote@gmail.com>
2017-11-29 14:42:50 -0800

    Comments system

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-	resources :articles
+	root to: 'articles#index'
+
+	resources :articles do
+		resources :comments
+	end
 	#The tutorial says I should see lots of comments, and should remove all but "Blogger::Application.routes.draw do".
 	#This is obviously different than what is here. Out of date? Make a mental note for later.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
